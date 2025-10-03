1122e276531bf6351a5e200e15856e5f0c6385e8
Zhaksylyk Imangali <user@MacBook-Air--imangali.local>
2025-10-03 11:50:32 +0500

    add the dto

--- src/features/auth/dto/auth_login.dto.ts
+++ src/features/auth/dto/auth_login.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty({description:"write your email",example:'example@gmail.com'})
   @IsString()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({description:'write your password',example:'qwerty123'})
   @IsString()
   @IsNotEmpty()
   @MinLength(6)
--- src/features/auth/dto/auth_register.dto.ts
+++ src/features/auth/dto/auth_register.dto.ts
@@ -1,15 +1,33 @@
-import { IsNumber, IsString, MaxLength, MinLength, minLength } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNumber,
+  IsString,
+  MaxLength,
+  MinLength,
+  minLength,
+} from 'class-validator';
 
-export class RegisterDto{
-    @IsString()
-    email:string
+export class RegisterDto {
+  @ApiProperty({
+    description: 'write your email',
+    example: 'example@gmail.com',
+  })
+  @IsString()
+  email: string;
 
-    @IsString()
-    @MinLength(7)
-    @MaxLength(20)
-    password:string
+  @ApiProperty({
+    description: 'write your password',
+    example: 'qwerty123',
+  })
+  @IsString()
+  @MinLength(7)
+  @MaxLength(20)
+  password: string;
 
-    @IsNumber()
-    phoneNumber:string
-    
+  @ApiProperty({
+    description: 'write your phonenumber',
+    example: '+77777777777',
+  })
+  @IsNumber()
+  phoneNumber: string;
 }
